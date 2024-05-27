d7c82864101b11bcbe3cd7aa2e5f44c33c1b637b
dtizzo <daynife.tizzo@nubank.com.br>
2024-05-27 15:51:52 -0300

    update description

--- ghlib.py
+++ ghlib.py
@@ -300,8 +300,7 @@ class AlertBase:
 #        return full_description
 
     def get_full_description(self):
-#        full_description = self.json.get("rule", {})
-        full_description = json.dumps(self.json, indent=4)
+        full_description = self.json.get("rule", {})
         if not full_description:
             full_description = "No description available."
         return full_description    
