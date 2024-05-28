c2453fa140424d7deaf27cc85323c355a15f2737
dtizzo <daynife.tizzo@nubank.com.br>
2024-05-28 12:27:09 -0300

    update description

--- sync.py
+++ sync.py
@@ -65,7 +65,7 @@ class Sync:
                 alert.get_key(),
                 alert.get_tool_name(),
                 alert.get_severity(),
-                alert.get_full_description(),
+                alert.get_full_description(resp),
             )
             newissue.adjust_state(alert.get_state())
             return alert.get_state()
