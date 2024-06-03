c6482adb90e25094036792dc651da97ee81d5373
Rafa Ziliao <rafaelh.ziliao@gmail.com>
2024-06-03 14:03:20 -0300

    Move methods to base class

--- ghlib.py
+++ ghlib.py
@@ -292,6 +292,12 @@ class AlertBase:
             security_severity_level = self.json.get("severity", "")
         return security_severity_level
 
+    def get_full_description(self):
+        return self.json["rule"].get("full_description", "")
+    
+    def get_help(self):
+        return self.json["rule"].get("help", "")
+     
     # def get_full_description(self):
     #     full_description = self.json.get("most_recent_instance", {}).get("message", {}).get("text", "")
     #     if not full_description:
@@ -332,12 +338,6 @@ class Alert(AlertBase):
             timeout=util.REQUEST_TIMEOUT,
         )
         resp.raise_for_status()
-
-    def get_full_description(self):
-        return self.json["rule"].get("full_description", "")
-    
-    def get_help(self):
-        return self.json["rule"].get("help", "")
 
 class Secret(AlertBase):
     def __init__(self, github_repo, json):
