16e29b75aeee7d5a5b0a8bd8af4ac157f7ef0214
dtizzo <daynife.tizzo@nubank.com.br>
2024-06-04 15:34:25 -0300

    update customfield

--- jiralib.py
+++ jiralib.py
@@ -211,11 +211,12 @@ class JiraProject:
             labels=self.labels,
             customfield_12957='Unknown',
             customfield_12927={'value': 'Unknown'},
+            customfield_13397={'value': (tool_mapping.get(tool_name, ''))}
         )
 
         jira_issue = JiraIssue(self, raw)
         jira_issue.set_exposure()
-        jira_issue.set_identification_source(tool_mapping.get(tool_name, ''))
+#        jira_issue.set_identification_source(tool_mapping.get(tool_name, ''))
         jira_issue.set_severity(severity_mapping.get(severity, ''))
         jira_issue.set_alert_reference(alert_url)
         jira_issue.set_repository()
