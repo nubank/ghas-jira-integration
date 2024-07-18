06b2429d04d4918dd1fbc37144c7dbf50611ceb0
dtizzo <daynife.tizzo@nubank.com.br>
2024-07-18 14:55:47 -0300

    update customfield

--- jiralib.py
+++ jiralib.py
@@ -216,7 +216,7 @@ class JiraProject:
             labels=self.labels,
             customfield_12957='Unknown',
             customfield_12927={'value': 'Unknown'},
-            customfield_13397={'value': (tool_mapping.get(tool_name, default_tool_name))},
+            customfield_13397={'value': 'null'},
             customfield_10457={'value': (severity_mapping.get(severity, default_severity))},
             customfield_12954={'value': 'Internal'},
             customfield_16751=['mini-meta-repo'],
