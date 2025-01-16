3696d6d0bf9d4f8cf967c88b8f99eb078624f3a9
dtizzo <daynife.tizzo@nubank.com.br>
2025-01-16 12:12:05 -0300

    update template

--- sync.py
+++ sync.py
@@ -69,7 +69,7 @@ class Sync:
                 alert.get_identification_date(),
                 alert.get_language(),
                 alert.get_cwe(),
-                alert.location(),
+                alert.get_location(),
                 alert.get_responsible_teams(),
                 )
 
