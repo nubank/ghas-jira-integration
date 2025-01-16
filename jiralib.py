aa42392625d000c5063464a3a1618c617778c1f5
dtizzo <daynife.tizzo@nubank.com.br>
2025-01-16 14:21:13 -0300

    update template

--- jiralib.py
+++ jiralib.py
@@ -36,6 +36,9 @@ owasp_mapping = {
 }
 
 DESC_TEMPLATE = """
+{long_desc}
+
+{full_description}
 
 *Location:* {location}
 
@@ -232,7 +235,7 @@ class JiraProject:
             issuetype={"name": "Vulnerability - General"},
             labels=self.labels,
             customfield_12957='Unknown',
-            customfield_12927={'value': 'Unknown'},
+]            customfield_12927={'value': 'Unknown'},
             customfield_13397={'value': (tool_mapping.get(tool_name, default_tool_name))},
             customfield_10457={'value': (severity_mapping.get(severity, default_severity))},
             customfield_12954={'value': 'Internal'},
