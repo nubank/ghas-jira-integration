c2dbdddd7c79307144a50ea50e8a5ba0532b0b48
dtizzo <daynife.tizzo@nubank.com.br>
2025-01-27 10:54:30 -0300

    add assignee to ticket

--- sync.py
+++ sync.py
@@ -72,7 +72,6 @@ class Sync:
                 alert.get_location(),
                 alert.get_responsible_teams(),
                 alert.get_team_members(),
-                alert.get_valid_assignee(),
                 )
 
             if newissue is None:
