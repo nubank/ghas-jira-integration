9c54eee3f3183348e1d906950384c03591e40fdd
dtizzo <daynife.tizzo@nubank.com.br>
2025-08-14 17:18:51 -0300

    enable assignee updates

--- sync.py
+++ sync.py
@@ -9,7 +9,7 @@ DIRECTION_G2J = 1
 DIRECTION_J2G = 2
 DIRECTION_BOTH = 3
 
-ENABLE_ASSIGNEE_UPDATES = os.getenv('ENABLE_ASSIGNEE_UPDATES', 'false').lower() in ('true', '1', 'yes')
+ENABLE_ASSIGNEE_UPDATES = os.getenv('ENABLE_ASSIGNEE_UPDATES', 'true').lower() in ('true', '1', 'yes')
 
 ASSIGN_TO_SECRET_AUTHOR = os.getenv('ASSIGN_TO_SECRET_AUTHOR', 'true').lower() in ('true', '1', 'yes')
 
